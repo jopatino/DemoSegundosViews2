88694b1acb7d7845f1c624e02c5d3cbf75f33c65
lorenaquint <lorenaquint@gmail.com>
2018-04-07 13:25:33 -0500

    tableView

--- DemoSegundosViews/DemoSegundosViews/MainPage.xaml.cs
+++ DemoSegundosViews/DemoSegundosViews/MainPage.xaml.cs
@@ -13,57 +13,57 @@ namespace DemoSegundosViews
 		{
 			InitializeComponent();
 		}
-        private void pickMusic_Changed(object sender, EventArgs e)
-        {
-            var elementoSeleccionado = pickMusic.SelectedItem.ToString();
-            DisplayAlert("Música", elementoSeleccionado, "Aceptar");
+        //private void pickMusic_Changed(object sender, EventArgs e)
+        //{
+        //    var elementoSeleccionado = pickMusic.SelectedItem.ToString();
+        //    DisplayAlert("Música", elementoSeleccionado, "Aceptar");
 
-        }
+        //}
 
-        private void btnSimular_Clicked(object sender, EventArgs e)
-        {
-            var progreso = progProceso.Progress;
-            if(progreso == 1)
-            {
-                progProceso.ProgressTo(.1, 250, Easing.Linear);
-            }
-            else
-            {
-                progProceso.ProgressTo(1, 250, Easing.Linear);
-            }
+        //private void btnSimular_Clicked(object sender, EventArgs e)
+        //{
+        //    var progreso = progProceso.Progress;
+        //    if(progreso == 1)
+        //    {
+        //        progProceso.ProgressTo(.1, 250, Easing.Linear);
+        //    }
+        //    else
+        //    {
+        //        progProceso.ProgressTo(1, 250, Easing.Linear);
+        //    }
             
-        }
+        //}
 
-        private void sbNombre_Pressed(object sender, EventArgs e)
-        {
-            DisplayAlert("Search Bar", "Estoy buscando...", "Aceptar");
-        }
-        private void sbNombre_Changed(object sender, EventArgs e)
-        {
-            DisplayAlert("Search Bar", "Estoy escribiendo...", "Aceptar");
-        }
+        //private void sbNombre_Pressed(object sender, EventArgs e)
+        //{
+        //    DisplayAlert("Search Bar", "Estoy buscando...", "Aceptar");
+        //}
+        //private void sbNombre_Changed(object sender, EventArgs e)
+        //{
+        //    DisplayAlert("Search Bar", "Estoy escribiendo...", "Aceptar");
+        //}
 
-        private void sldEdad_Changed(object sender, EventArgs e)
-        {
-            lbEdad.Text = sldEdad.Value.ToString();
-        }
+        //private void sldEdad_Changed(object sender, EventArgs e)
+        //{
+        //    lbEdad.Text = sldEdad.Value.ToString();
+        //}
 
-        private void stpCantidad_Changed(object sender, EventArgs e)
-        {
-            lbCantidad.Text = stpCantidad.Value.ToString();
-        }
+        //private void stpCantidad_Changed(object sender, EventArgs e)
+        //{
+        //    lbCantidad.Text = stpCantidad.Value.ToString();
+        //}
 
-        private void swiDec_Toggle(object sender, EventArgs e)
-        {
-            if (swiDec.IsToggled)
-            {
-                DisplayAlert("Mensaje", "Está Activado", "Aceptar");
-            }
-            else
-            {
-                DisplayAlert("Mensaje", "Está Desactivado", "Aceptar");
-            }
-        }
+        //private void swiDec_Toggle(object sender, EventArgs e)
+        //{
+        //    if (swiDec.IsToggled)
+        //    {
+        //        DisplayAlert("Mensaje", "Está Activado", "Aceptar");
+        //    }
+        //    else
+        //    {
+        //        DisplayAlert("Mensaje", "Está Desactivado", "Aceptar");
+        //    }
+        //}
 
     }
 }
